37f78b542f2ab722a85d74c3217c82ff2be8a125
messaoudisony <messaoudi.sonia89@gmail.com>
2024-04-01 16:30:25 +0200

    add props.1 passer les propos

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,17 +1,6 @@
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
-
-function PokemonCard() {
-    const pokemon = pokemonList[0];
+function PokemonCard({pokemon}) {
+    
 
     return(
         <div>
