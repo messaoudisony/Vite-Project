0117a7f2cab4b35f2d2f3171aaba4d60a61fd53e
messaoudisony <messaoudi.sonia89@gmail.com>
2024-04-01 14:46:22 +0200

    Creat first components PokemonCard

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,15 @@
+function PokemonCard() {
+    return(
+        <div>
+        <figure>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
+        </figure>
+        <figcaption>
+            <p>Bulbasaur</p>
+        </figcaption>
+
+        </div>
+    )
+}
+
+export default PokemonCard
