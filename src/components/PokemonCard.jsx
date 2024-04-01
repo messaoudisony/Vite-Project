4d39a44509421748aafe1767c9596dd42f651d68
messaoudisony <messaoudi.sonia89@gmail.com>
2024-04-01 15:35:28 +0200

    add branch components2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,29 @@
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 function PokemonCard() {
+    const pokemon = pokemonList[0];
+
     return(
         <div>
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
+            {pokemon.imgSrc ?
+            <img src={pokemon.imgSrc}/>
+            :
+            <p>???</p>
+            }
         </figure>
         <figcaption>
-            <p>Bulbasaur</p>
+            <p>{pokemon.name}</p>
         </figcaption>
 
         </div>
